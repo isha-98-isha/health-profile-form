af7fa9b9910a15b0abf93ca0916e2672a80bea7d
isha-98-isha <ishachaudhari98@gmail.com>
2026-08-22 15:54:03 +0530

    Added Login And Signup Pages with Match UI

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import React from 'react';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import Dashboard from './pages/Dashboard/dashboard';
 import Onboarding from './pages/Onboardings/Onboarding';
+import Login from './pages/Login/login';
+import Signup from './pages/Signup/signup';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <Routes>
-          <Route path="/" element={<Dashboard />} />
+          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route
             path="/onboarding"
